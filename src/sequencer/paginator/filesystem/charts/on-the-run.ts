963507d82058328367ff1f584ba5a593c3cb9b15
jankrum <professionalninety@gmail.com>
2024-10-19 12:12:52 -0700

    Changed on the run to be a little shorter

--- src/sequencer/paginator/filesystem/charts/on-the-run.ts
+++ src/sequencer/paginator/filesystem/charts/on-the-run.ts
@@ -1,68 +1,77 @@
-import { Chart, BufferEvent, BufferComputeEvent, BufferEventType, } from '../../../../types.ts'
-import { convertPitchNameToMidiNumber, sitOut, convertBpmToMpb } from '../helper.ts'
+import { Chart, BufferEvent, BufferComputeEvent, BufferEventType, BufferNoteOnEvent, BufferNoteOffEvent, } from '../../../../types.ts'
+import Part from '../../../playbacker/band/part/part.ts'
+import { convertPitchNameToMidiNumber, convertBpmToMpb, pipe, finish, PipeOperation, } from '../helper.ts'
 
-// const pitches = [40, 43, 45, 43, 50, 48, 50, 52]
-const pitches = ['E3', 'G3', 'A3', 'G3', 'D4', 'C4', 'D4', 'E4'].map(convertPitchNameToMidiNumber)
+const pitches = ['E3', 'G3', 'A3', 'G3', 'D4', 'C4', 'D4', 'E4']
 const runDuration = 2
 const stepDuration = runDuration / pitches.length
 const millisecondsPerBeat = convertBpmToMpb(165)
 
-const chart: Chart = {
-    title: "On the Run",
-    compose: ({ bass, drum, keys, lead }): BufferEvent[] => {
+function repeatRun(lead: Part): PipeOperation {
+    // Doing this here to avoid doing it in the callback
+    const midiPitches = pitches.map(convertPitchNameToMidiNumber)
+
+    return (initialState) => {
+        // This control determines if the run should repeat
         const repeatControl = lead.controller.getOptionControl('Repeat again: ', ['No', 'Yes'], '!')
 
-        function makeRun(position: number): BufferEvent[] {
-            const noteOnAndOffEvents = pitches.map((pitch, index) => {
-                const startPosition = position + (index * stepDuration)
-                return [
-                    {
-                        time: startPosition * millisecondsPerBeat,
-                        type: BufferEventType.NoteOn,
+        // Given a position, create a compute event that may create a run
+        const maybeMakeRun = (position: number): BufferComputeEvent => ({
+            time: (position * millisecondsPerBeat) - 100,  // Schedule the run to start 100ms before the position
+            type: BufferEventType.Compute,
+            callback: (buffer) => {
+                if (repeatControl.value) {
+                    // If the run should repeat, create note on and off events for each pitch
+                    const noteOnAndOffEvents = midiPitches.map((pitch, index): [BufferNoteOnEvent, BufferNoteOffEvent] => {
+                        const startPosition = position + (index * stepDuration)
+                        return [
+                            {
+                                time: startPosition * millisecondsPerBeat,
+                                type: BufferEventType.NoteOn,
+                                part: lead,
+                                pitch,
+                                velocity: 0x7F,
+                            },
+                            {
+                                time: (startPosition + stepDuration - 0.05) * millisecondsPerBeat,
+                                type: BufferEventType.NoteOff,
+                                part: lead,
+                                pitch,
+                            },
+                        ]
+                    }).flat()
+
+                    // Push the note on and off events and a compute event for the next run
+                    buffer.push(
+                        ...noteOnAndOffEvents,
+                        maybeMakeRun(position + runDuration),
+                    )
+                } else {
+                    // If the run should not repeat, create a finish event
+                    buffer.push({
+                        time: position * millisecondsPerBeat,
+                        type: BufferEventType.Finish,
                         part: lead,
-                        pitch,
-                        velocity: 0x7F,
-                    },
-                    {
-                        time: (startPosition + stepDuration - 0.05) * millisecondsPerBeat,
-                        type: BufferEventType.NoteOff,
-                        part: lead,
-                        pitch,
-                    },
-                ]
-            }).flat() as BufferEvent[]
-
-            const computeEvent = maybeMakeRun(position + runDuration)
-
-            return [
-                ...noteOnAndOffEvents,
-                computeEvent,
-            ]
-        }
-
-        function maybeMakeRun(position: number): BufferComputeEvent {
-            return {
-                time: (position - 0.1) * millisecondsPerBeat,
-                type: BufferEventType.Compute,
-                callback: (buffer) => {
-                    if (repeatControl.value) {
-                        buffer.push(...makeRun(position))
-                    } else {
-                        buffer.push({
-                            time: position * millisecondsPerBeat,
-                            type: BufferEventType.Finish,
-                            part: lead,
-                        })
-                    }
+                    })
                 }
             }
-        }
+        })
 
-        return [
-            ...sitOut(bass, drum, keys),
-            maybeMakeRun(0),
-        ]
-    },
+        return Object.assign({}, initialState, {
+            bufferEvents: [
+                ...initialState.bufferEvents,
+                maybeMakeRun(0),
+            ],
+        })
+    }
+}
+
+const chart: Chart = {
+    title: 'On the Run',
+    compose: ({ bass, drum, keys, lead }): BufferEvent[] => pipe(
+        finish(bass, drum, keys),
+        repeatRun(lead),
+    ),
 }
 
 export default chart
