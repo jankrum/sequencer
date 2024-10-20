4db9b7cfb6651a09e1b1df394f35fa7fed08f7ea
jankrum <professionalninety@gmail.com>
2024-10-19 21:22:05 -0700

    Made the file for My Favorite Things

--- src/sequencer/paginator/filesystem/setlist.ts
+++ src/sequencer/paginator/filesystem/setlist.ts
@@ -3,7 +3,8 @@ import helloWorld from './charts/hello-world.ts'
 import bassScale from './charts/bass-scale.ts'
 import twinkleTwinkleLittleStar from './charts/twinkle-twinkle-little-star.ts'
 import onTheRun from './charts/on-the-run.ts'
+import myFavoriteThings from './charts/my-favorite-things.ts'
 
-const setlist: Chart[] = [helloWorld, bassScale, twinkleTwinkleLittleStar, onTheRun]
+const setlist: Chart[] = [helloWorld, bassScale, twinkleTwinkleLittleStar, onTheRun, myFavoriteThings,]
 
 export default setlist
